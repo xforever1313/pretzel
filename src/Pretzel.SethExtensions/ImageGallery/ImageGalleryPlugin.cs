86f73a684632388550725a91ebfef87758c03323
Seth Hendrick <seth@shendrick.net>
2024-02-10 13:05:09 -0500

    Skip if image does not exist and print a warning, include pages as well

--- src/Pretzel.SethExtensions/ImageGallery/ImageGalleryPlugin.cs
+++ src/Pretzel.SethExtensions/ImageGallery/ImageGalleryPlugin.cs
@@ -48,101 +48,118 @@ namespace Pretzel.SethExtensions.ImageGallery
 
             foreach( Page page in context.Posts )
             {
-                if( page.Bag.ContainsKey( imageGallerySetting ) == false )
+                AddGallery( context, thumbNailFolder, page );
+            }
+
+            foreach( Page page in new List<Page>( context.Pages ) )
+            {
+                AddGallery( context, thumbNailFolder, page );
+            }
+        }
+
+        private void AddGallery( SiteContext context, DirectoryInfo thumbNailFolder, Page page )
+        {
+            if( page.Bag.ContainsKey( imageGallerySetting ) == false )
+            {
+                return;
+            }
+
+            string? imageGallerySettingValue = page.Bag[imageGallerySetting].ToString();
+            if( string.IsNullOrEmpty( imageGallerySettingValue ) )
+            {
+                throw new InvalidOperationException( $"No value specified in {imageGallerySetting} on page {page.Id}" );
+            }
+
+            var imageGalleryFile = new FileInfo(
+                Path.Combine( context.SourceFolder, imageGallerySettingValue )
+            );
+
+            ImageGalleryConfig config = ImageGalleryConfigExtensions.FromXml( imageGalleryFile );
+            config.Validate();
+
+            var linkHelper = new LinkHelper();
+
+            var imageContentList = new List<ImageInfoContext>();
+
+            Console.WriteLine( $"Generating thumbnails for image gallery on page: {page.Id}..." );
+            Parallel.ForEach(
+                config.ImageInfo,
+                ( ImageInfo imageInfo ) =>
                 {
-                    continue;
-                }
+                    string originalFile = Path.Combine(
+                        context.SourceFolder,
+                        config.InputImageDirectory,
+                        imageInfo.FileName
+                    );
 
-                string? imageGallerySettingValue = page.Bag[imageGallerySetting].ToString();
-                if( string.IsNullOrEmpty( imageGallerySettingValue ) )
-                {
-                    throw new InvalidOperationException( $"No value specified in {imageGallerySetting} on page {page.Id}" );
-                }
+                    // GetFullPath normalizes separator characters.
+                    originalFile = Path.GetFullPath( originalFile );
 
-                var imageGalleryFile = new FileInfo(
-                    Path.Combine( context.SourceFolder, imageGallerySettingValue )
-                );
-
-                ImageGalleryConfig config = ImageGalleryConfigExtensions.FromXml( imageGalleryFile );
-                config.Validate();
-
-                var linkHelper = new LinkHelper();
-
-                var imageContentList = new List<ImageInfoContext>();
-
-                Console.WriteLine( $"Generating thumbnails for image gallery on page: {page.Id}..." );
-                Parallel.ForEach(
-                    config.ImageInfo,
-                    ( ImageInfo imageInfo ) =>
+                    if( Path.Exists( originalFile ) == false )
                     {
-                        string originalFile = Path.Combine(
-                            context.SourceFolder,
-                            config.InputImageDirectory,
-                            imageInfo.FileName
-                        );
+                        Console.WriteLine( $"\t-Warning: '{originalFile}' does not exist, skipping thumbnail creation." );
+                        return;
+                    }
 
-                        // GetFullPath normalizes separator characters.
-                        originalFile = Path.GetFullPath( originalFile );
+                    using( var fileStream = new FileStream( originalFile, FileMode.Open, FileAccess.Read ) )
+                    using( var image = new MagickImage( fileStream ) )
+                    {
+                        int thumbnailHeight = (int)Math.Round( image.Height * config.ThumbnailScale );
+                        int thumbnailWidth = (int)Math.Round( image.Width * config.ThumbnailScale );
+                        var size = new MagickGeometry( thumbnailWidth, thumbnailHeight );
 
-                        using( var fileStream = new FileStream( originalFile, FileMode.Open, FileAccess.Read ) )
-                        using( var image = new MagickImage( fileStream ) )
+                        image.Resize( size );
+
+                        string inFile = Path.Combine( thumbNailFolder.FullName, imageInfo.ThumbnailFileName );
+                        image.Write( inFile );
+
+                        var thumbnailPage = new Page
                         {
-                            int thumbnailHeight = (int)Math.Round( image.Height * config.ThumbnailScale );
-                            int thumbnailWidth = (int)Math.Round( image.Width * config.ThumbnailScale );
-                            var size = new MagickGeometry( thumbnailWidth, thumbnailHeight );
+                            Id = $"{config.Id}-{imageInfo.FileName}",
+                            File = inFile,
+                            Filepath = Path.Combine( context.OutputFolder, config.ThumbnailOutputFolder, imageInfo.ThumbnailFileName ),
+                            OutputFile = Path.Combine( context.OutputFolder, config.ThumbnailOutputFolder, imageInfo.ThumbnailFileName )
+                        };
+                        thumbnailPage.Bag["layout"] = "nil";
 
-                            image.Resize( size );
-
-                            string inFile = Path.Combine( thumbNailFolder.FullName, imageInfo.ThumbnailFileName );
-                            image.Write( inFile );
-
-                            var thumbnailPage = new Page
+                        Page? originalPhotoPage = null;
+                        lock( context )
+                        {
+                            thumbnailPage.Url = linkHelper.EvaluateLink( context, thumbnailPage );
+                            context.Pages.Add( thumbnailPage );
+                            foreach( Page pageToLook in context.Pages )
                             {
-                                Id = $"{config.Id}-{imageInfo.FileName}",
-                                File = inFile,
-                                Filepath = Path.Combine( context.OutputFolder, config.ThumbnailOutputFolder, imageInfo.ThumbnailFileName ),
-                                OutputFile = Path.Combine( context.OutputFolder, config.ThumbnailOutputFolder, imageInfo.ThumbnailFileName )
-                            };
-
-                            Page? originalPhotoPage = null;
-                            lock( context )
-                            {
-                                thumbnailPage.Url = linkHelper.EvaluateLink( context, thumbnailPage );
-                                context.Pages.Add( thumbnailPage );
-                                foreach( Page pageToLook in context.Pages )
+                                if( pageToLook.File == originalFile )
                                 {
-                                    if( pageToLook.File == originalFile )
-                                    {
-                                        originalPhotoPage = pageToLook;
-                                        break;
-                                    }
+                                    originalPhotoPage = pageToLook;
+                                    break;
                                 }
                             }
+                        }
 
-                            if( originalPhotoPage is null )
-                            {
-                                throw new InvalidOperationException(
-                                    $"Can not locate original image to thumbnail: {imageInfo.FileName}"
-                                );
-                            }
-
-                            var imageInfoContext = new ImageInfoContext(
-                                linkHelper.EvaluateLink( context, originalPhotoPage ),
-                                thumbnailPage.Url,
-                                thumbnailWidth,
-                                thumbnailHeight,
-                                imageInfo
+                        if( originalPhotoPage is null )
+                        {
+                            throw new InvalidOperationException(
+                                $"Can not locate original image to thumbnail: {imageInfo.FileName}"
                             );
-                            lock( imageContentList )
-                            {
-                                imageContentList.Add( imageInfoContext );
-                            }
+                        }
+
+                        var imageInfoContext = new ImageInfoContext(
+                            linkHelper.EvaluateLink( context, originalPhotoPage ),
+                            thumbnailPage.Url,
+                            thumbnailWidth,
+                            thumbnailHeight,
+                            imageInfo
+                        );
+                        lock( imageContentList )
+                        {
+                            imageContentList.Add( imageInfoContext );
                         }
                     }
-                );
-                page.Bag[ImageGalleryDataKey] = imageContentList.OrderBy( i => i.ImageInfo.Index );
-                Console.WriteLine( $"Generating thumbnails for image gallery on page: {page.Id}...Done!" );
-            }
+                }
+            );
+            page.Bag[ImageGalleryDataKey] = imageContentList.OrderBy( i => i.ImageInfo.Index );
+            Console.WriteLine( $"Generating thumbnails for image gallery on page: {page.Id}...Done!" );
         }
     }
 }
