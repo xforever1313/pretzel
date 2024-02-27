04b5d34d23872a22cf95e0591ced70d114f37fcb
Seth Hendrick <seth@shendrick.net>
2024-02-26 19:56:38 -0500

    Made the URL extension public

--- src/Pretzel.SethExtensions/SethMarkdownEngine.cs
+++ src/Pretzel.SethExtensions/SethMarkdownEngine.cs
@@ -22,100 +22,10 @@ namespace Pretzel.SethExtensions
         {
             var pipeline = new MarkdownPipelineBuilder()
                 .UseAdvancedExtensions()
-                .Use<MarkdownExtension>()
+                .Use<SethUrlMarkdownExtension>()
                 .Build();
 
             return Markdown.ToHtml( source, pipeline );
-        }
-
-        private class MarkdownExtension : IMarkdownExtension
-        {
-            public void Setup( MarkdownPipelineBuilder pipeline )
-            {
-            }
-
-            public void Setup( MarkdownPipeline pipeline, IMarkdownRenderer renderer )
-            {
-                var htmlRender = renderer as HtmlRenderer;
-                if( htmlRender is null )
-                {
-                    return;
-                }
-
-                var linkRender = renderer.ObjectRenderers.FindExact<LinkInlineRenderer>();
-                if( linkRender is not null )
-                {
-                    linkRender.TryWriters.Remove( TryLinkInlineRenderer );
-                    linkRender.TryWriters.Add( TryLinkInlineRenderer );
-                }
-
-                var autoLinkRender = renderer.ObjectRenderers.Find<AutolinkInlineRenderer>();
-                if( autoLinkRender is not null )
-                {
-                    autoLinkRender.TryWriters.Remove( TryAutoLinkInlineRenderer );
-                    autoLinkRender.TryWriters.Add( TryAutoLinkInlineRenderer );
-                }
-            }
-
-            private bool TryLinkInlineRenderer( HtmlRenderer renderer, LinkInline linkInline )
-            {
-                if( linkInline.Url is null )
-                {
-                    return false;
-                }
-
-                // Only process absolute Uri
-                if(
-                    ( Uri.TryCreate( linkInline.Url, UriKind.RelativeOrAbsolute, out Uri? uri ) == false ) ||
-                    ( uri.IsAbsoluteUri == false )
-                )
-                {
-                    return false;
-                }
-
-                linkInline.SetAttributes(
-                    new HtmlAttributes()
-                    {
-                        Properties = new List<KeyValuePair<string, string?>>()
-                        {
-                            new KeyValuePair<string, string?>( "target", "_blank" ),
-                            new KeyValuePair<string, string?>( "rel", SethHtmlFormatter.ATagRelProperties )
-                        }
-                    }
-                );
-
-                return false;
-            }
-
-            private bool TryAutoLinkInlineRenderer( HtmlRenderer renderer, AutolinkInline linkInline )
-            {
-                if( linkInline.Url is null )
-                {
-                    return false;
-                }
-
-                // Only process absolute Uri
-                if(
-                    ( Uri.TryCreate( linkInline.Url, UriKind.RelativeOrAbsolute, out Uri? uri ) == false ) ||
-                    ( uri.IsAbsoluteUri == false )
-                )
-                {
-                    return false;
-                }
-
-                linkInline.SetAttributes(
-                    new HtmlAttributes()
-                    {
-                        Properties = new List<KeyValuePair<string, string?>>()
-                        {
-                            new KeyValuePair<string, string?>( "target", "_blank" ),
-                            new KeyValuePair<string, string?>( "rel", SethHtmlFormatter.ATagRelProperties )
-                        }
-                    }
-                );
-
-                return false;
-            }
         }
     }
 }
