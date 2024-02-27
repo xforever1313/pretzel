a52b29406ba72523aa42763f5f1ed9e5904f9ea1
Seth Hendrick <seth@shendrick.net>
2024-02-26 22:06:14 -0500

    Added ability to not export markdown engine

--- src/Pretzel.SethExtensions/SethMarkdownEngine.cs
+++ src/Pretzel.SethExtensions/SethMarkdownEngine.cs
@@ -15,7 +15,9 @@ using Pretzel.Logic.Extensibility;
 
 namespace Pretzel.SethExtensions
 {
+    #if !NO_EXPORT_SETH_MARKDOWN_ENGINE
     [Export( typeof( ILightweightMarkupEngine ) )]
+    #endif
     public sealed class SethMarkdownEngine : ILightweightMarkupEngine
     {
         public string Convert( string source )
